5358a6260224a4feacab9e3e1569a2de989a345d
NabilBouazzaoui <73765566+NabilBouazzaoui@users.noreply.github.com>
2022-01-08 13:56:55 -0500

    ajout du index global

---
+++ index.js
@@ -0,0 +1,12 @@
+import Avatar from './Avatar/Badge';
+import Bouton from './Bouton/Bouton';
+import Breadcrumbs from './Breadcrumbs/Breadcrumbs';
+import Checkbox from './Checkbox/Checkboite';
+import Header from './Header/Header';
+import link from './Link/Link';
+import Select from './Select/Select';
+import Spinner from './Spinner/Spinner';
+import Switch from './Switch/Switched';
+import Input from './Text-input/Input';
+
+export{Avatar, Bouton, Breadcrumbs, Checkbox, Header, link, Select, Spinner, Switch, Switch, Input}
